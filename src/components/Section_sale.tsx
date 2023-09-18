eb6012366392be87f0ef25d7968b59e55aaef251
Ilya Stafeev <stafeev-ilya@bk.ru>
2023-09-18 14:16:34 +0700

    18.09.2023 14:16

--- src/components/Section_sale.tsx
+++ src/components/Section_sale.tsx
@@ -4,7 +4,15 @@ const Section_sale = () => {
     return (
         <section className='Section-sale'>
             <div className="container">
-                
+                <div className="row">
+                    <div className="row1">
+                        <div className='heading'>Earn up to $25 worth of crypto</div>
+                        <p className='desc'>Discover how specific cryptocurrencies work — and get a bit of each crypto to try out for yourself.</p>
+                    </div>
+                    <div className="row2">                        
+                        <button><a href="">Create Account</a></button>                       
+                    </div>
+                </div>
             </div>
         </section>
     );
